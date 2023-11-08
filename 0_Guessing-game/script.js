42a2907f05c340dff8d17548f7ee63023f173c6b
Sieglinde <sieglindevwb@gmail.com>
2023-11-08 10:11:53 +0100

    add folder with html and js file and todo's

---
+++ 0_Guessing-game/script.js
@@ -0,0 +1,13 @@
+/* 
+Number Guessing Game 
+Computer = 1 to 10
+User has to guess the number 
+The user gets 3 chances to do so
+*/
+
+// TODO: A secret number will be generated -> should be between 1 and 24 -> With the while loop
+// TODO: A user can enter a guess (add HTML element)
+// TODO: When he or she pushes the play button, the number will be compared tot the secret number -> if... else condition
+// TODO: POPS UP a alert message ->
+//      - Awesome! You number (actual number) was correct. You can be named many things, hungry not being one of them.
+//      - Bummer... You guessed (guess) and the secret number was (secret number).
