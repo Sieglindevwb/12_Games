e7232a63ed5fccf2536ecdc1ce4bd4dad8e9b01c
Sieglinde <sieglindevwb@gmail.com>
2023-11-08 12:25:55 +0100

    add css and html to the first game

---
+++ 1_Rock-paper-scissors/script.js
@@ -0,0 +1,6 @@
+
+// TODO: Have three buttons that the player can press to pick their weapon of choice (feel free to use emoji's for this)
+// TODO: Have a play button to have the computer pick one randomly
+// TODO: Announce the winner
+//     - No alert box, add a message to your html
+//     - Possible outcomes: draw (when both chose the same thing) or player won/lost
