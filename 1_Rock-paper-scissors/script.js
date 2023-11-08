4465cdeb703e6bf8aad8db5fe7cfd095e4d7da9f
Sieglinde <sieglindevwb@gmail.com>
2023-11-08 15:11:17 +0100

    add the javascript buttons and messages

--- 1_Rock-paper-scissors/script.js
+++ 1_Rock-paper-scissors/script.js
@@ -1,6 +1,47 @@
+const playButton = document.querySelector(".playBtn")
+
+playButton.addEventListener("click", (event) =>{
+    const choices = document.querySelectorAll(".choiceBtn")
+
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    const computerChoice = choices[randomIndex].dataset.choice;
+
+    console.log(computerChoice);
+
+    const resultMessage = document.querySelector(".result-message");
+
+    choices.forEach (choice => {
+        choice.addEventListener("click", (event) =>{
+            const playerChoice = event.target.dataset.choice;
+            determineWinner(playerChoice);
+
+            function determineWinner(playerChoice) {
+                if (playerChoice === computerChoice) {
+                    resultMessage.innerHTML = "It's a draw!";
+                } else if
+                ((playerChoice === "rock" && computerChoice === "scissors") ||
+                (playerChoice === "paper" && computerChoice === "rock") ||
+                (playerChoice === "scissors" && computerChoice === "paper")) {
+                // Player wins
+                // Update the result message
+                resultMessage.innerHTML = "You win!";
+                } else {
+                // Computer wins
+                // Update the result message
+                resultMessage.innerHTML = "Computer wins!";
+                }
+            }
+        } )
+    })
+    
+});
 
 // TODO: Have three buttons that the player can press to pick their weapon of choice (feel free to use emoji's for this)
 // TODO: Have a play button to have the computer pick one randomly
 // TODO: Announce the winner
 //     - No alert box, add a message to your html
 //     - Possible outcomes: draw (when both chose the same thing) or player won/lost
+// TODO: Figure out a way to know when the "play" button was clicked (tip: you'll need `events` in JavaScript for that, and you'll need to `handle` those)
+//      4. Next up, we want to determine the computer's choice
+//         - How many choices does the computer have? What can be a suitable datatype to store multiple things?
+//         - How can you select a random element from these choices? The math options in JavaScript might help you here
