109b2e13b4fb8028eef2848f48e16c6005232eb8
Sieglinde <sieglindevwb@gmail.com>
2023-11-08 11:41:55 +0100

    add eventlistener with the if and else conditions

--- 0_Guessing-game/script.js
+++ 0_Guessing-game/script.js
@@ -12,14 +12,24 @@ while (!secretNumber || secretNumber < 1 || secretNumber > 24) {
 
 console.log(secretNumber);
 
+const guessNumber = document.querySelector("form")
 
-// console.log(secretNumber);
+guessNumber.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const userGuess = parseInt(event.target.number.value);
+
+    if (userGuess === secretNumber) {
+    alert(`Awesome! You number ${userGuess} was correct. You can be named many things, hungry not being one of them.`);
+    } else {
+    alert(`Bummer... You guessed ${userGuess} and the secret number was ${secretNumber}.`);}
+});
+// console.log();
 
 
 
-// TODO: A secret number will be generated -> should be between 1 and 24 -> With the while loop
+// A secret number will be generated -> should be between 1 and 24 -> With the while loop
 // TODO: A user can enter a guess (add HTML element)
-// TODO: When he or she pushes the play button, the number will be compared tot the secret number -> if... else condition
+// TODO: When he or she pushes the play button, the number will be compared to the secret number -> if... else condition
 // TODO: POPS UP a alert message ->
 //      - Awesome! You number (actual number) was correct. You can be named many things, hungry not being one of them.
 //      - Bummer... You guessed (guess) and the secret number was (secret number).
